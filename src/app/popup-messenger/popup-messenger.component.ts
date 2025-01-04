813a7dd0af3e2419040076c49305e362f83a365f
NadiaSun <nadia.kurakina12@gmail.com>
2025-01-04 17:10:48 +0200

    add email

--- src/app/popup-messenger/popup-messenger.component.ts
+++ src/app/popup-messenger/popup-messenger.component.ts
@@ -39,7 +39,7 @@ export class PopupMessengerComponent implements OnInit {
     const data: Email = {
       from_name:  this.form.value.name,
       from_email: 'whitefox1331@gmail.com',
-      message:  this.form.value.phoneInt
+      message:  this.form.value.phoneInt.e164Number
       ? `Phone: ${this.form.value.phoneInt.e164Number}, id-site: ${this.popupService.card.id}`
       : ''
     };
